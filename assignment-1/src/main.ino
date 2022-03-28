adbe152992925431fb8fc4ce9892baddc99a2fad
Alessandro Mazzoli <alemazzoli97@gmail.com>
2022-03-28 15:20:35 +0200

    cleaned main.ino

--- assignment-1/src/main.ino
+++ assignment-1/src/main.ino
@@ -1,25 +1,17 @@
 #include <Arduino.h>
 #include "game/game.h"
-///////////////////////////////////////////////////////////////////////////////
-// Configuration
-///////////////////////////////////////////////////////////////////////////////
 
 uint8_t potentiometerPin = A0;
 uint8_t ballLedPins[4] = { 2, 3, 4, 5 };
 uint8_t buttonPins[4] = { 6, 7, 8, 9 };
 uint8_t fadingLedPin = 10;
 
-///////////////////////////////////////////////////////////////////////////////
-// Game creation
-///////////////////////////////////////////////////////////////////////////////
 Game* game = new Game(fadingLedPin, potentiometerPin, ballLedPins, buttonPins);
 
 void setup() {
   Serial.begin(9600);
   game->setup();
 }
-
-unsigned long lastIterationTimestamp = 0;
 
 void loop() {
   game->computeIteration();
