47e88819eb137e6275af73f660cdcaf1e74f2e9a
Alessandro Mazzoli <alemazzoli97@gmail.com>
2022-07-12 12:47:23 +0200

    complete RedisPubSubWrapper

--- assignment-3/app/garden-service/serial-component/main.py
+++ assignment-3/app/garden-service/serial-component/main.py
@@ -1,13 +1,30 @@
-import redis
+import redis, serial
 from threading import Thread
 from time import sleep
 from redis_pubsub_wrapper import RedisPubSubWrapper
 
  
-db = redis.Redis("redis")
+db = redis.Redis("localhost")
 pubsub = RedisPubSubWrapper(db)
 
+def update_strategy_handler(message):
+    print(f"Update strategy : {message}")
 
+def update_status_handler(message):
+    print(f"Update status : {message}")
+
+def serial_loop():
+    connection = serial.Serial(port = 'COM3', baudrate = 9600)
+    while True:
+        message = connection.readline()
+        message_type, content = message.split(":")
 
 if __name__ == '__main__':
-    pass
+    pubsub.subscribe(topic = "update-strategy", handler = update_strategy_handler)
+    pubsub.subscribe(topic = "update-status", handler = update_status_handler)
+    #serial_loop()
+    while True:
+        pubsub.publish("update-strategy", "test")
+        sleep(.5)
+        pubsub.publish("update-status", "test")
+        sleep(.5)
--- assignment-3/app/garden-service/serial-component/redis_pubsub_wrapper.py
+++ assignment-3/app/garden-service/serial-component/redis_pubsub_wrapper.py
@@ -1,25 +1,32 @@
+from threading import Thread
+from time import sleep 
 class RedisPubSubWrapper():
-    
-    def __init__(self, database: redis.Redis):
-        self.database: redis.Redis = database
+    def __init__(self, database):
+        self.database = database
         self.pubsub = self.database.pubsub()
-        self.channelMap = []
+        self.channelMap = dict()
+        self._start_subscribe_thread()
 
-    def _start_subscribe_thread():
-        self.subscribe_thread = Thread(target=_subscribe_splitter_thread)
+    def _start_subscribe_thread(self):
+        self.subscribe_thread = Thread(target=self._subscribe_splitter_thread)
         self.subscribe_thread.start()
 
-    def _subscribe_splitter_thread():
+    def _subscribe_splitter_thread(self):
         while True:
             message = self.pubsub.get_message()
             if message:
-                topic = message.topic
-                self.channelMap[topic]()
+                type = message["type"]
+                if (type == "subscribe"):
+                    continue
+                topic = message["channel"].decode()
+                data = message["data"].decode()
+                self.channelMap[topic](data)
             sleep(0.01)
 
-    def subscribe(topic: str, handler):
+    def subscribe(self, topic, handler):
         self.channelMap[topic] = handler
         self.pubsub.subscribe(topic)
+        self.pubsub.get_message()
 
-    def publish(topic: str, message: str):
+    def publish(self, topic, message):
         self.database.publish(topic, message)
