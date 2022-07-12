818bc3d7b0f194c37305249a33d7ab703ea8465e
manuandru <manuandru987@gmail.com>
2022-07-12 18:02:32 +0200

    finish mqtt component

--- assignment-3/app/garden-service/mqtt-component/config.py
+++ assignment-3/app/garden-service/mqtt-component/config.py
@@ -1,3 +1,13 @@
-class Topic:
-    sensorboard = "smart-garden/sensorboard"
-    status = "smart-garden/status"
+class Config:
+    class mqtt:
+        host = "broker-mqtt"
+        sensorboard = "smart-garden/sensorboard"
+        status = "smart-garden/status"
+    
+    class redis:
+        host = "redis"
+        sensorboard = "update-sensorboard"
+        status = "update-status"
+
+    temperature = "temperature"
+    light = "light"
--- assignment-3/app/garden-service/mqtt-component/main.py
+++ assignment-3/app/garden-service/mqtt-component/main.py
@@ -1,27 +1,43 @@
 import paho.mqtt.client as mqtt
 import redis
 import json
-from config import Topic
+from threading import Thread
+from config import Config
 
-db = redis.Redis(host='localhost')
+db = redis.Redis(host=Config.redis.host)
 mqtt = mqtt.Client()
 
-def on_subscribe(client, userdata, mid, granted_qos):
-    print("Subscribed: " + str(mid) + " " + str(granted_qos))
-
-def on_message(client, userdata, msg):
+def on_mqtt_message(client, userdata, msg):
     data = json.loads(str(msg.payload.decode("utf-8")))
-    if msg.topic == Topic.sensorboard and "temperature" in data and "light" in data:
-        db.set("temperature", data["temperature"])
-        db.set("light", data["light"])
+    if msg.topic == Config.mqtt.sensorboard and Config.temperature in data and Config.light in data:
+        db.set(Config.temperature, data[Config.temperature])
+        db.set(Config.light, data[Config.light])
+        db.publish(
+            Config.redis.sensorboard,
+            json.dumps({
+                Config.temperature: data[Config.temperature], 
+                Config.light: data[Config.light]
+            })
+        )
 
+def redis_listener_thread():
+    pubsub = db.pubsub()
+    pubsub.subscribe(Config.redis.status)
+    while True:
+        message = pubsub.get_message()
+        if message and message["type"] == "message":
+            mqtt.publish(Config.mqtt.status, message["data"].decode("utf-8"))
 
 if __name__ == '__main__':
     try:
-        mqtt.on_subscribe = on_subscribe
-        mqtt.on_message = on_message
-        mqtt.connect("localhost")
-        mqtt.subscribe(Topic.sensorboard, qos=2)
+        # create thread
+        thread = Thread(target=redis_listener_thread)
+        thread.start()
+
+        # connect to mqtt broker
+        mqtt.on_message = on_mqtt_message
+        mqtt.connect(Config.mqtt.host)
+        mqtt.subscribe(Config.mqtt.sensorboard, qos=2)
         mqtt.loop_forever()
     except Exception as exception:
         print('connect to client error')
